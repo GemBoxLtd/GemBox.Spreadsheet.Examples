f0295d75ebbac68daeda423d0d1d6f490d03ea63
GemBox Dev Team <info@gemboxsoftware.com>
2020-05-18 13:38:42 +0200

    Updated PDF Digital Signature example.

--- C#/Protection/PDF Digital Signature/Program.cs
+++ C#/Protection/PDF Digital Signature/Program.cs
@@ -1,23 +1,101 @@
+using GemBox.Pdf.Forms;
+using GemBox.Pdf.Security;
 using GemBox.Spreadsheet;
 
 class Program
 {
     static void Main()
     {
+        PAdES_B_B();
+
+        PAdES_B_LTA();
+    }
+
+    static void PAdES_B_B()
+    {
         // If using Professional version, put your serial key below.
         SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
 
         var workbook = ExcelFile.Load("SimpleTemplate.xlsx");
 
+        // Create visual representation of digital signature at the beginning of the worksheet.
+        var signature = workbook.Worksheets[0].Pictures.Add("GemBoxSignature.png", "B2");
+
         var options = new PdfSaveOptions()
         {
             DigitalSignature =
             {
-                CertificatePath = "GemBoxExampleExplorer.pfx",
-                CertificatePassword = "GemBoxPassword"
+                CertificatePath = "GemBoxECDsa521.pfx",
+                CertificatePassword = "GemBoxPassword",
+                Signature = signature,
+                IsAdvancedElectronicSignature = true
             }
         };
 
         workbook.Save("PDF Digital Signature.pdf", options);
+    }
+
+    static void PAdES_B_LTA()
+    {
+        // If using Professional version, put your serial key below.
+        SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+        var workbook = ExcelFile.Load("SimpleTemplate.xlsx");
+
+        // Create visual representation of digital signature at the beginning of the first worksheet.
+        var signature = workbook.Worksheets[0].Pictures.Add("GemBoxSignature.png", "B2");
+
+        // If using Professional version, put your serial key below.
+        GemBox.Pdf.ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+        // Get a digital ID from PKCS#12/PFX file.
+        var digitalId = new PdfDigitalId("GemBoxECDsa521.pfx", "GemBoxPassword");
+
+        // Create a PDF signer that will create PAdES B-LTA level signature.
+        var signer = new PdfSigner(digitalId);
+
+        // PdfSigner should create CAdES-equivalent signature.
+        signer.SignatureFormat = PdfSignatureFormat.CAdES;
+
+        // PdfSigner will embed a timestamp created by freeTSA.org Time Stamp Authority in the signature.
+        signer.Timestamper = new PdfTimestamper("https://freetsa.org/tsr");
+
+        // Make sure that all properties specified on PdfSigner are according to PAdES B-LTA level.
+        signer.SignatureLevel = PdfSignatureLevel.PAdES_B_LTA;
+
+        // Inject PdfSigner from GemBox.Pdf into
+        // PdfDigitalSignatureSaveOptions from GemBox.Spreadsheet.
+        var signatureOptions = PdfDigitalSignatureSaveOptions.FromSigner(
+            () => signer.SignatureFormat.ToString(),
+            () => signer.EstimatedSignatureContentsLength,
+            signer.ComputeSignature);
+
+        signatureOptions.Signature = signature;
+
+        var options = new PdfSaveOptions()
+        {
+            DigitalSignature = signatureOptions
+        };
+
+        workbook.Save("PAdES B-LTA.pdf", options);
+
+        using (var pdfDocument = GemBox.Pdf.PdfDocument.Load("PAdES B-LTA.pdf"))
+        {
+            var signatureField = (PdfSignatureField)pdfDocument.Form.Fields[0];
+
+            // Download validation-related information for the signature and the signature's timestamp and embed it in the PDF file.
+            // This will make the signature "LTV enabled".
+            pdfDocument.SecurityStore.AddValidationInfo(signatureField.Value);
+
+            // Add an invisible signature field to the PDF document that will hold the document timestamp.
+            var timestampField = pdfDocument.Form.Fields.AddSignature();
+
+            // Initiate timestamping of a PDF file with the specified timestamper.
+            timestampField.Timestamp(signer.Timestamper);
+
+            // Save any changes done to the PDF file that were done since the last time Save was called and
+            // finish timestamping of a PDF file.
+            pdfDocument.Save();
+        }
     }
 }
--- VB.NET/Protection/PDF Digital Signature/Program.vb
+++ VB.NET/Protection/PDF Digital Signature/Program.vb
@@ -1,20 +1,101 @@
 Imports GemBox.Spreadsheet
+Imports GemBox.Pdf.Forms
+Imports GemBox.Pdf.Security
 
 Module Program
 
     Sub Main()
+
+        PAdES_B_B()
+
+        PAdES_B_LTA()
+    End Sub
+
+    Sub PAdES_B_B()
 
         ' If using Professional version, put your serial key below.
         SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY")
 
         Dim workbook = ExcelFile.Load("SimpleTemplate.xlsx")
 
-        Dim options = New PdfSaveOptions()
-        Dim digitalSignature = options.DigitalSignature
+        ' Create visual representation of digital signature at the beginning of the worksheet.
+        Dim signature = workbook.Worksheets(0).Pictures.Add("GemBoxSignature.png", "B2")
 
-        digitalSignature.CertificatePath = "GemBoxExampleExplorer.pfx"
-        digitalSignature.CertificatePassword = "GemBoxPassword"
+        Dim options As New PdfSaveOptions() With
+        {
+            .DigitalSignature = New PdfDigitalSignatureSaveOptions() With
+            {
+                .CertificatePath = "GemBoxECDsa521.pfx",
+                .CertificatePassword = "GemBoxPassword",
+                .Signature = signature,
+                .IsAdvancedElectronicSignature = True
+            }
+        }
 
         workbook.Save("PDF Digital Signature.pdf", options)
+    End Sub
+
+    Sub PAdES_B_LTA()
+
+        ' If using Professional version, put your serial key below.
+        SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY")
+
+        Dim workbook = ExcelFile.Load("SimpleTemplate.xlsx")
+
+        ' Create visual representation of digital signature at the beginning of the first worksheet.
+        Dim signature = workbook.Worksheets(0).Pictures.Add("GemBoxSignature.png", "B2")
+
+        ' If using Professional version, put your serial key below.
+        GemBox.Pdf.ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+
+        ' Get a digital ID from PKCS#12/PFX file.
+        Dim digitalId = New PdfDigitalId("GemBoxECDsa521.pfx", "GemBoxPassword")
+
+        ' Create a PDF signer that will create PAdES B-LTA level signature.
+        Dim signer = New PdfSigner(digitalId)
+
+        ' PdfSigner should create CAdES-equivalent signature.
+        signer.SignatureFormat = PdfSignatureFormat.CAdES
+
+        ' PdfSigner will embed a timestamp created by freeTSA.org Time Stamp Authority in the signature.
+        signer.Timestamper = New PdfTimestamper("https://freetsa.org/tsr")
+
+        ' Make sure that all properties specified on PdfSigner are according to PAdES B-LTA level.
+        signer.SignatureLevel = PdfSignatureLevel.PAdES_B_LTA
+
+        ' Inject PdfSigner from GemBox.Pdf into
+        ' PdfDigitalSignatureSaveOptions from GemBox.Spreadsheet.
+        Dim signatureOptions = PdfDigitalSignatureSaveOptions.FromSigner(
+            Function() signer.SignatureFormat.ToString(),
+            Function() signer.EstimatedSignatureContentsLength,
+            Function(pdfFileStream) signer.ComputeSignature(pdfFileStream))
+
+        signatureOptions.Signature = signature
+
+        Dim options = New PdfSaveOptions() With
+        {
+            .DigitalSignature = signatureOptions
+        }
+
+        workbook.Save("PAdES B-LTA.pdf", options)
+
+        Using pdfDocument = GemBox.Pdf.PdfDocument.Load("PAdES B-LTA.pdf")
+
+            Dim signatureField = CType(pdfDocument.Form.Fields(0), PdfSignatureField)
+
+            ' Download validation-related information for the signature and the signature's timestamp and embed it in the PDF file.
+            ' This will make the signature "LTV enabled".
+            pdfDocument.SecurityStore.AddValidationInfo(signatureField.Value)
+
+            ' Add an invisible signature field to the PDF document that will hold the document timestamp.
+            Dim timestampField = pdfDocument.Form.Fields.AddSignature()
+
+            ' Initiate timestamping of a PDF file with the specified timestamper.
+            timestampField.Timestamp(signer.Timestamper)
+
+            ' Save any changes done to the PDF file that were done since the last time Save was called and
+            ' finish timestamping of a PDF file.
+            pdfDocument.Save()
+        End Using
     End Sub
 End Module
