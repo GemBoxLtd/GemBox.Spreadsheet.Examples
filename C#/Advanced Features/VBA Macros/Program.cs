a9c5ba8bb67d5aeb1179d158c4c68dcb93967eb5
GemBox Dev Team <info@gemboxsoftware.com>
2023-01-18 13:15:08 +0100

    Updated "Digital Signature" example and added "VBA Macros" example.

---
+++ C#/Advanced Features/VBA Macros/Program.cs
@@ -0,0 +1,47 @@
+using GemBox.Spreadsheet;
+using GemBox.Spreadsheet.Vba;
+
+class Program
+{
+    static void Main()
+    {
+        // If using the Professional version, put your serial key below.
+        SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+        Example1();
+        Example2();
+    }
+
+    static void Example1()
+    {
+        var workbook = new ExcelFile();
+        var worksheet = workbook.Worksheets.Add("Sheet1");
+
+        // Create the module.
+        VbaModule vbaModule = workbook.VbaProject.Modules.Add(worksheet);
+        vbaModule.Code =
+@"Sub Button1_Click()
+    MsgBox ""Hello World!""
+End Sub";
+
+        // Create a button to assign macro.
+        var button = worksheet.FormControls.AddButton("Click Me!", "B2", 100, 15, LengthUnit.Point);
+        // Assign the macro.
+        button.SetMacro(vbaModule, "Button1_Click");
+
+        // Save the workbook as macro-enabled Excel file.
+        workbook.Save("AddVbaModule.xlsm");
+    }
+
+    static void Example2()
+    {
+        var workbook = ExcelFile.Load("SampleVba.xlsm");
+
+        // Get the module.
+        VbaModule vbaModule = workbook.VbaProject.Modules["Module1"];
+        // Update text for the popup message.
+        vbaModule.Code = vbaModule.Code.Replace("Hello world!", "Hello from GemBox.Spreadsheet!");
+
+        workbook.Save("UpdateVbaModule.xlsm");
+    }
+}
---
+++ VB.NET/Advanced Features/VBA Macros/Program.vb
@@ -0,0 +1,46 @@
+Imports GemBox.Spreadsheet
+Imports GemBox.Spreadsheet.Vba
+
+Module Program
+
+    Sub Main()
+
+        ' If using the Professional version, put your serial key below.
+        SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY")
+
+        Example1()
+        Example2()
+
+    End Sub
+
+    Sub Example1()
+        Dim workbook As New ExcelFile()
+        Dim worksheet = workbook.Worksheets.Add("Sheet1")
+
+        ' Create the module.
+        Dim vbaModule As VbaModule = workbook.VbaProject.Modules.Add(worksheet)
+        vbaModule.Code =
+"Sub Button1_Click()
+    MsgBox ""Hello World!""
+End Sub"
+
+        ' Create a button to assign macro.
+        Dim button = worksheet.FormControls.AddButton("Click Me!", "B2", 100, 15, LengthUnit.Point)
+        ' Assign the macro.
+        button.SetMacro(vbaModule, "Button1_Click")
+
+        ' Save the workbook as macro-enabled Excel file.
+        workbook.Save("AddVbaModule.xlsm")
+    End Sub
+
+    Sub Example2()
+        Dim workbook = ExcelFile.Load("SampleVba.xlsm")
+
+        ' Get the module.
+        Dim vbaModule As VbaModule = workbook.VbaProject.Modules("Module1")
+        ' Update text for the popup message.
+        vbaModule.Code = vbaModule.Code.Replace("Hello world!", "Hello from GemBox.Spreadsheet!")
+
+        workbook.Save("UpdateVbaModule.xlsm")
+    End Sub
+End Module
