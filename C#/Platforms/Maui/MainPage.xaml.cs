37d7f23ca784c210b18370e4d1ad88dbcbe0095e
GemBox Dev Team <info@gemboxsoftware.com>
2023-02-18 19:10:21 +0300

    Added example Maui project.

---
+++ C#/Platforms/Maui/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using GemBox.Spreadsheet;
+
+namespace SpreadsheetMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        static MainPage()
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<string> CreateWorkbookAsync()
+        {
+            var workbook = new ExcelFile();
+            var worksheet = workbook.Worksheets.Add("Sheet1");
+
+            foreach (var cell in table.Root[0].Cast<EntryCell>())
+                worksheet.Cells[cell.Label].Value = cell.Text;
+
+            worksheet.Columns["A"].AutoFit();
+
+            using var stream = new MemoryStream();
+            using (var imageStream = await FileSystem.OpenAppPackageFileAsync("dices.png"))
+                await imageStream.CopyToAsync(stream);
+            worksheet.Pictures.Add(stream, ExcelPictureFormat.Png, "C1", "E5");
+
+            var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Example.pdf");
+
+            workbook.Save(filePath);
+
+            return filePath;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            button.IsEnabled = false;
+            activity.IsRunning = true;
+
+            var filePath = await CreateWorkbookAsync();
+            await Launcher.OpenAsync(new OpenFileRequest(Path.GetFileName(filePath), new ReadOnlyFile(filePath)));
+
+            activity.IsRunning = false;
+            button.IsEnabled = true;
+        }
+    }
+}
