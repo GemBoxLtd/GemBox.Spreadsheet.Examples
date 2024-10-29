56154d813ccd4db6b19343bf767241e8e945a6ce
Mario at GemBox <mario.zorica@gemboxsoftware.com>
2024-10-29 07:12:45 +0100

    Updated "Fonts" example.

--- C#/Features/Fonts/Program.cs
+++ C#/Features/Fonts/Program.cs
@@ -7,13 +7,13 @@ class Program
         // If using the Professional version, put your serial key below.
         SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
 
+        // Set the directory path where the component will look for additional font files.
+        // The "MyFonts" targets the subdirectory in the current directory, so besides the installed fonts,
+        // the component will be able to use the fonts within the specified directory.
+        FontSettings.FontsBaseDirectory = "MyFonts";
+
         var workbook = new ExcelFile();
         var worksheet = workbook.Worksheets.Add("Private Fonts");
-
-        // Set the directory path where the component will look for additional font files.
-        // The "." targets the current directory, so besides the installed fonts,
-        // the component will be able to use the fonts within the specified directory.
-        FontSettings.FontsBaseDirectory = ".";
 
         worksheet.Parent.Styles.Normal.Font.Name = "Almonte Snow";
         worksheet.Parent.Styles.Normal.Font.Size = 48 * 20;
